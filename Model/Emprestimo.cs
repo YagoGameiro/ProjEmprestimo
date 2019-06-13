03e7bb43d2b89e19a7f4bb8701f66d93a424002d
YagoGameiro <48812787+YagoGameiro@users.noreply.github.com>
2019-06-13 19:59:39 -0300

    Add files via upload

---
+++ Model/Emprestimo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Emprestimo
+    {
+        #region Propriedades
+
+        public int  Id { get; set; }
+        public string   Descricao { get; set; }
+        public string Valor { get; set; }
+        public string   Data_Emprestimo { get; set; }
+        public string Juros { get; set; }
+        public string Parcela { get; set; }
+
+        public Cidade Cidade { get; set; }
+
+        #endregion
+    }
+}
